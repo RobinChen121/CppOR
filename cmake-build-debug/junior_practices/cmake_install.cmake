a6442f5e6ffc74e78565bd81dd170cea5ed0e170
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-06-09 09:33:57 +0100

    update

--- cmake-build-debug/junior_practices/cmake_install.cmake
+++ cmake-build-debug/junior_practices/cmake_install.cmake
@@ -37,8 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.3.5/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("D:/chenzhen/CppOR/cmake-build-debug/_deps/matplotlib_cpp-build/cmake_install.cmake")
-endif()
-
