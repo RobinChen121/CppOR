f72f1295c8cd8131630eb59fe50b29986d304356
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-08-03 23:59:19 +0100

    update

--- linear_programming/my_solver/simplex.h
+++ linear_programming/my_solver/simplex.h
@@ -10,11 +10,29 @@
 #define SIMPLEX_H
 #include <vector>
 
+enum class Comparison {
+  LessOrEqual,
+  Equal,
+  GreaterOrEqual
+};
+
+enum class VarSign {
+  NonNegative,  // ≥ 0
+  NonPositive   // ≤ 0
+};
+
 class Simplex {
 private:
-    std::vector<std::vector<double> > tableau; // 单纯形表
-    int rows, cols;
-    std::vector<int> basicVars;
+  std::vector<double> obj_coefs;
+  bool obj_senses;
+  std::vector<std::vector<double>> conLHSs;
+  std::vector<double> conRHSs;
+  std::vector<Comparison> conComparisons;
+  std::vector<VarSign> varSigns;
+
+  std::vector<std::vector<double> > tableau; // 单纯形表
+  int rows, cols;
+  std::vector<int> basicVars;
 
     // 找到主列（进入变量）
     int findPivotColumn() const;
@@ -27,6 +45,8 @@ private:
 
 public:
     explicit Simplex(const std::vector<std::vector<double> > &initialTableau);
+
+    void inputObjCoef() const;
 
     void solve();
 
