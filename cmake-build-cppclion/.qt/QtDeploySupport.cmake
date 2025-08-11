287481d0767d4e9c7fcbcaa6ae732fe3bc1d4f50
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2025-08-11 19:40:36 +0100

    update

--- cmake-build-cppclion/.qt/QtDeploySupport.cmake
+++ cmake-build-cppclion/.qt/QtDeploySupport.cmake
@@ -38,7 +38,7 @@ set(__QT_DEPLOY_IMPL_DIR "/Users/zhenchen/CLionProjects/CppOR/cmake-build-cppcli
 set(__QT_DEPLOY_VERBOSE "")
 set(__QT_CMAKE_EXPORT_NAMESPACE "Qt6")
 set(__QT_DEPLOY_GENERATOR_IS_MULTI_CONFIG "0")
-set(__QT_DEPLOY_ACTIVE_CONFIG "Debug")
+set(__QT_DEPLOY_ACTIVE_CONFIG "Release")
 set(__QT_NO_CREATE_VERSIONLESS_FUNCTIONS "")
 set(__QT_DEFAULT_MAJOR_VERSION "6")
 set(__QT_DEPLOY_QT_ADDITIONAL_PACKAGES_PREFIX_PATH "")
@@ -59,7 +59,7 @@ set(__QT_DEPLOY_QT_DEBUG_POSTFIX "")
 # Define the CMake commands to be made available during deployment.
 set(__qt_deploy_support_files
     "/Users/zhenchen/CLionProjects/CppOR/cmake-build-cppclion/.qt/QtDeployTargets.cmake"
-    "/opt/homebrew/Cellar/qt/6.8.2/lib/cmake/Qt6Core/Qt6CoreDeploySupport.cmake"
+    "/opt/homebrew/Cellar/qt/6.9.1/lib/cmake/Qt6Core/Qt6CoreDeploySupport.cmake"
 )
 foreach(__qt_deploy_support_file IN LISTS __qt_deploy_support_files)
     include("${__qt_deploy_support_file}")
