24a6ce74390369d9a2b6063548adcf8c6b76710a
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2026-03-29 23:57:49 +0100

    update

--- linear_programming/chen_solver2/simplex.cpp
+++ linear_programming/chen_solver2/simplex.cpp
@@ -7,3 +7,55 @@
  */
 
 #include "simplex.h"
+
+#include <iostream>
+
+CSC Simplex::denseToCSC(const std::vector<std::vector<double>> &A) {
+  const int m = static_cast<int>(A.size());    // numer of row
+  const int n = static_cast<int>(A[0].size()); // number of column
+
+  const int nnz = m * n;
+  if (m * n > 1e6) // to-do
+    return {};
+  CSC csc;
+  csc.non_zero_values.reserve(nnz);
+  csc.col_start_end.push_back(0);
+
+  for (int j = 0; j < n; j++) { // scan from column
+    for (int i = 0; i < m; i++) {
+      if (std::abs(A[i][j]) > eps) {
+        csc.non_zero_values.push_back(A[i][j]);
+        csc.row_indices.push_back(i);
+      }
+    }
+    csc.col_start_end.push_back(static_cast<int>(csc.non_zero_values.size()));
+  }
+
+  return csc;
+}
+
+void Simplex::printCSC(const CSC &csc) {
+  std::cout << "values: ";
+  for (const auto v : csc.non_zero_values)
+    std::cout << v << " ";
+  std::cout << std::endl;
+
+  std::cout << "row_idx: ";
+  for (const auto r : csc.row_indices)
+    std::cout << r << " ";
+  std::cout << std::endl;
+
+  std::cout << "col_ptr: ";
+  for (const auto c : csc.col_start_end)
+    std::cout << c << " ";
+  std::cout << std::endl;
+}
+
+int main() {
+  const std::vector<std::vector<double>> A = {{1, 0, 2}, {0, 3, 0}, {4, 0, 5}};
+
+  const CSC csc = Simplex::denseToCSC(A);
+  Simplex::printCSC(csc);
+
+  return 0;
+}
--- linear_programming/chen_solver2/simplex.h
+++ linear_programming/chen_solver2/simplex.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Created by Zhen Chen on 2026/3/20.
  * Email: chen.zhen5526@gmail.com
  * Description: This simplex algorithm aims to process sparse matrix.
@@ -11,7 +11,22 @@
 
 #include <vector>
 
-constexpr double epsilon = 1e-6;
+constexpr double eps = 1e-12;
+
+// compressed sparse column
+struct CSC {
+  std::vector<double> non_zero_values;
+  std::vector<int> row_indices;   // row indices for the non-zero values
+  std::vector<int> col_start_end; // column start and end indices in the non-zero values
+
+  CSC() = default; // 空对象方便临时使用
+  // nnz = 预计非零元素个数
+  // cols = 矩阵列数
+  // 工业级时最好估计非零个数，从而提前分配内存
+  // array 也可以直接用数值初始化，此时元素都为0
+  CSC(const int nnz, const int cols)
+      : non_zero_values(nnz), row_indices(nnz), col_start_end(cols + 1) {}
+};
 
 class Simplex {
   std::vector<double> obj_coe;
@@ -21,6 +36,12 @@ class Simplex {
   std::vector<int> constraint_sense; // 0:<=, 1: >=, 2: =
   std::vector<int> var_sign;         // 0: >=, 1: <=, 2: unsigned
   int solution_status = {3};         // 0 optimal, 1 unbounded, 2 infeasible, 3 unsolved
+
+public:
+  Simplex();
+
+  static CSC denseToCSC(const std::vector<std::vector<double>> &A);
+  static void printCSC(const CSC &csc);
 };
 
 #endif // WORKFORCE_SIMPLEX_H
