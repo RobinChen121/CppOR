16e1be54c6adcc3898fe6d6a60a2613f459b17d2
RobinChen121 <40953071+RobinChen121@users.noreply.github.com>
2026-08-31 21:09:24 +0100

    Update workforce_plan.h

--- stochastic_inventory/workforce/workforce_plan.h
+++ stochastic_inventory/workforce/workforce_plan.h
@@ -26,7 +26,7 @@ class WorkforcePlan {
   Direction direction = Direction::BACKWARD; // backward will be parallel computing
   ToComputeGy to_compute_gy = ToComputeGy::False;
 
-  std::vector<double> turnover_rates = {0.1, 0.3, 0.5, 0.5, 0.3, 0.1};
+  std::vector<double> turnover_rates = {0.1, 0.1, 0.1, 0.3, 0.3, 0.3, 0.5, 0.5, 0.5, 0.3, 0.3, 0.3};
   size_t T = turnover_rates.size();
 
   int initial_workers = 0;
